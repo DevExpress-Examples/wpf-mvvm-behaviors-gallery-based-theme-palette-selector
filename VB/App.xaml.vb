09af7dbfa500c5c363778bb29ebd4dfa35cd1604
DevExpressExampleBot <fakeEmail>
2021-09-23 16:01:49 +0300

    Source auto update [skip ci]

---
+++ VB/App.xaml.vb
@@ -0,0 +1,18 @@
+Imports DevExpress.Xpf.Core
+Imports System.Windows
+Imports System.Windows.Media
+
+Namespace ThemeHelperWPF
+
+    Public Partial Class App
+        Inherits Application
+
+        Public Sub New()
+            Theme.RegisterPredefinedPaletteThemes()
+            Dim customPalette = New ThemePalette("CustomPalette")
+            customPalette.SetColor("Foreground", Colors.Red)
+            Dim customTheme = Theme.CreateTheme(customPalette, Theme.Office2016ColorfulSE)
+            Theme.RegisterTheme(customTheme)
+        End Sub
+    End Class
+End Namespace
